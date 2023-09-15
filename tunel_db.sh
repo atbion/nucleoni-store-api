c504cf8f7f4731da7ebf10c8184331639c3bf2a8
Yadisnel Galvez Velázquez <yadisnel@gmail.com>
2023-09-15 19:04:07 +0200

    Nucleoni

--- tunel_db.sh
+++ tunel_db.sh
@@ -1,1 +1,1 @@
-ssh -i /Users/yadisnelgalvezvelazquez/Desktop/Work/Pem/atbion-root-key-pair.pem -L 5432:nucleoni-cluster-dev.cluster-cbmqvpcbt2de.eu-west-1.rds.amazonaws.com:5432 ec2-user@54.78.197.165 -N
+ssh -i /Users/yadisnelgalvezvelazquez/Desktop/Work/Pem/atbion-root-key-pair.pem -L 5432:nucleoni-cluster-dev.cluster-cbmqvpcbt2de.eu-west-1.rds.amazonaws.com:5432 ec2-user@52.50.247.166 -N
--- tunel_memcached.sh
+++ tunel_memcached.sh
@@ -1,1 +1,1 @@
-ssh -i /Users/yadisnelgalvezvelazquez/Desktop/Work/Pem/atbion-root-key-pair.pem -L 11211:cache-cluster-dev.yzokb1.0001.euw1.cache.amazonaws.com:11211 ec2-user@54.78.181.180 -N
+ssh -i /Users/yadisnelgalvezvelazquez/Desktop/Work/Pem/atbion-root-key-pair.pem -L 11211:cache-cluster-dev.yzokb1.0001.euw1.cache.amazonaws.com:11211 ec2-user@52.50.247.166 -N
