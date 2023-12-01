5c1b483e7d42d9d5f2697a51817a7140468977fc
Yadisnel Galvez Velazquez <yadisnel@gmail.com>
2023-12-01 18:48:45 +0100

    saleor settings

--- nucleoni.py
+++ nucleoni.py
@@ -16,7 +16,7 @@ def system_handler():
     print(f"SYSTEM HANDLER PROVISIONING_DATABASE_OPERATION: {os.environ.get('PROVISIONING_DATABASE_OPERATION')}")
     print(f"SYSTEM HANDLER CUSTOMER_ID: {os.environ.get('CUSTOMER_ID')}")
     print(f"SYSTEM HANDLER SETUP_DATABASE_STEP_TASK_TOKEN: {os.environ.get('SETUP_DATABASE_STEP_TASK_TOKEN')}")
-    os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
+    os.environ["DJANGO_SETTINGS_MODULE"] = "saleor.settings"
     import django
 
     django.setup()
