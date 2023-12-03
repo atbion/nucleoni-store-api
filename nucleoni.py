bbf0c856c7f8e806bc69706aa56ec2db98fc2006
Yadisnel Galvez Velazquez <yadisnel@gmail.com>
2023-12-03 12:32:06 +0100

    docker entry point

--- nucleoni.py
+++ nucleoni.py
@@ -5,6 +5,10 @@ Copyright (c) 2023 - present Atbion<atbion.com>
 Yadisnel Galvez Velazquez <yadisnel@atbion.com>
 """
 import os
+import logging
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class OperationTypeNotFound(Exception):
@@ -13,9 +17,11 @@ class OperationTypeNotFound(Exception):
 
 
 def system_handler():
-    print(f"SYSTEM HANDLER PROVISIONING_DATABASE_OPERATION: {os.environ.get('PROVISIONING_DATABASE_OPERATION')}")
-    print(f"SYSTEM HANDLER CUSTOMER_ID: {os.environ.get('CUSTOMER_ID')}")
-    print(f"SYSTEM HANDLER SETUP_DATABASE_STEP_TASK_TOKEN: {os.environ.get('SETUP_DATABASE_STEP_TASK_TOKEN')}")
+    logger.info(
+        f"SYSTEM HANDLER PROVISIONING_DATABASE_OPERATION: {os.environ.get('PROVISIONING_DATABASE_OPERATION')}")
+    logger.info(f"SYSTEM HANDLER CUSTOMER_ID: {os.environ.get('CUSTOMER_ID')}")
+    logger.info(
+        f"SYSTEM HANDLER SETUP_DATABASE_STEP_TASK_TOKEN: {os.environ.get('SETUP_DATABASE_STEP_TASK_TOKEN')}")
     os.environ["DJANGO_SETTINGS_MODULE"] = "saleor.settings"
     import django
 
@@ -25,3 +31,6 @@ def system_handler():
         "statusCode": 200,
     }
 
+
+if __name__ == "__main__":
+    system_handler()
