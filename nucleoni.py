312ccc6b7540163d787a36318f0415ad1e17ba5d
Yadisnel Galvez Velazquez <yadisnel@gmail.com>
2023-12-04 17:56:46 +0100

    fix output

--- nucleoni.py
+++ nucleoni.py
@@ -4,6 +4,7 @@
 Copyright (c) 2023 - present Atbion<atbion.com>
 Yadisnel Galvez Velazquez <yadisnel@atbion.com>
 """
+import json
 import os
 import logging
 
@@ -54,9 +55,9 @@ def system_handler():
         step_function_client = boto3.client("stepfunctions")
         step_function_client.send_task_success(
             taskToken=os.environ.get("SETUP_DATABASE_STEP_TASK_TOKEN"),
-            output={
+            output=json.dumps({
                 "status": f"Database migrated: {customer_provisioning_db.database_name}",
-            },
+            }),
         )
 
     return {
